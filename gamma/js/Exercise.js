ab808ac9b68b10aa27058a83d7a5ccccf8843132
Eun Chong Ju <vevsing@gmail.com>
2022-09-14 02:39:15 +0900

    Update Exercise.js

--- gamma/js/Exercise.js
+++ gamma/js/Exercise.js
@@ -2,18 +2,18 @@
 
 
 // 이 클래스는 운동할 때만 실행
-// 들숨시간(=endTime), 숨참시간, 날숨시간(=startTime), 자막의 갯수, 자막의 반복횟수, 들-날 그룹여부, 숨참시 자막표시 여부, 
+// 들숨시간(=endTime), 숨참시간, 날숨시간(=startTime), 자막의 갯수, 자막의 반복횟수, 들-날 그룹여부, 숨참시 자막표시 여부,
 var Exercise = function(inhaleTime, holdTime, exhaleTime, subtitleLength, subtitleCycle, inexGroup, subtitleOnHold) {
 	var startTime = exhaleTime;
 	var endTime = inhaleTime;
-	
+
 	// 숨참시 자막표시 여부는 숨참시간이 0이면 값은 무효가 됨
 	// 들-날 그룹여부가 true면 들-날이 통합되어 들+날은 하나로 간주하게 된다.
-	// 들-날 그룹여부가 false면 들-날이 분리되어 두개로 간주한다. 
-	// 
-	
-	
-	
+	// 들-날 그룹여부가 false면 들-날이 분리되어 두개로 간주한다.
+	//
+
+
+
 }
 
 
@@ -21,13 +21,13 @@ function CCMaker(parascript, inhaleTime, exhaleTime, holdingTime, repeat, merge,
 	var startParas = parascript.getStartParas();
 	var endParas = parascript.getEndParas();
 	var parasList = parascript.get();
-	
+
 	var parasLength = (merge) ? parasList.length : parasList.length * 2;
-	
+
 	var ccArr = [];
-	
+
 	ccArr.push(startParas);
-	
+
 	for (var i = 0; i < parasList.length; i++) {
 		// 홀드 타임 표시 허용 시 inhale, exhale 사이에 자막이 들어간다.
 		// 그리고 표시 불허 시 inhale, exhale 사이에 자막이 들어가지 않아 비어있는 문자열이 들어간다.
@@ -35,11 +35,46 @@ function CCMaker(parascript, inhaleTime, exhaleTime, holdingTime, repeat, merge,
 		// 그리고 repeat가 나오면 방금까지 한 것을 반복해서 표시해야 한다.
 		// 아 코딩하기 귀찮
 	}
-	
+
 	ccArr.push(endParas);
-	
+
 	return ccArr;
 }
+
+
+function getTimeArr(paras, subtitleLength, repeat, useHoldingTime, inexGroup, useHoldingSubtitle) {
+	var timeElement = {
+		time, subtitle
+	};
+	var timeArr = [];
+	
+}
+
+function getSubtitleArr(paras, subtitleLength, repeat, useHoldingTime, inexGroup, useHoldingSubtitle) {
+	var arr = [];
+	// start Paras, end Paras는 원래 개별적으로 따로 표시됨
+	if (useHoldingTime) {
+		if (useHoldingSubtitle) {	// 숨참기 시간 사용, 숨참기 자막 표시
+
+		}
+		else {	// 숨참기 시간 사용, 숨참기 자막 표시안함
+
+		}
+	}
+	else {
+		if (inexGroup) {	// 숨참기 시간 사용안함, 들이쉬기-내쉬기 자막 일치(병합)
+
+		}
+		else {	// 숨참기 시간 사용안함, 들이쉬기-내쉬기 자막 불일치(분리)
+
+		}
+	}
+}
+
+
+
+
+
 
 
 
@@ -54,9 +89,9 @@ function activeService() {
 	var subtitleCycle = 2;
 	var inexGroup = true;
 	var subtitleOnHold = true;
-	
+
 	service = new Exercise(inhaleTime, holdTime, exhaleTime, subtitleLength, subtitleCycle, inexGroup, subtitleOnHold);
-	
+
 }
 
 function clickLungs() {
@@ -77,8 +112,8 @@ function init() {
 
 function getExerciseTime(inhaleTime, holdTime, exhaleTime, subtitleLength, subtitleCycle, inexGroup, subtitleOnHold) {
 	var time = inhaleTime + exhaleTime;
-	
-	
+
+
 	return time;
 }
 
@@ -95,9 +130,3 @@ function backToIntro() {
 	// 만약에 운동중이라면 운동을 멈추고 인트로로 화면전환을 실행해야 할 것이다.
 	showIntro();
 }
-
-
-
-
-
-
