9eb7a28b5aa5cf7723be614529d8b891bb52b9cc
Eun Chong Ju <vevsing@gmail.com>
2024-03-10 16:31:44 +0900

    Create testEditor.js

---
+++ testEditor.js
@@ -0,0 +1,20 @@
+
+var Editor = function() {
+	var sys = null;
+	
+	this.newSystem = function() {
+		sys = new Sys();
+	}
+	
+	
+	this.loadScript = function() {
+		
+	}
+};
+
+function selectScript(n) {
+	var scriptsArr = sys.loadScript();
+	for (var i = 0; i < scriptsArr.length; i++) {
+		
+	}
+}
